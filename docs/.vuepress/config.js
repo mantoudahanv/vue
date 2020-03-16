e24584bc4a89fe4b62f9a851dae479540e77ad29
e <mantoudahanv@gmail.com>
2020-03-16 19:03:41 +0800

    deploy

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,67 @@
+module.exports = {
+    title:'山己子木',
+    description: '山己子木,vuepress文档',
+    locales: {
+      '/':{
+        lang:'zh-CN',
+      }
+    },
+    head: [
+      ['link', { rel: 'icon', href: '/favicon.ico' }]
+    ],
+    base:'/vue/',
+    themeConfig: {
+      //调整github项目地址
+      repo: 'https://github.com/mantoudahanv/vue.github.io',
+      editLinks: false,
+      editLinkText: '编辑文档！',
+
+      // 最后更新时间
+      // lastUpdated: '上次更新', // string | boolean
+      // avbar: false  用于禁用所有页面导航栏
+      
+      //sidebar:true,//自动生成侧边栏
+        nav: [
+          { text: '主页', link: '/' },
+          { text: 'Guide', link: '/guide/' },
+          {
+            text: 'Languages',
+            ariaLabel: 'Language Menu',
+            //嵌套下拉分组
+            items: [
+              { text: 'Chinese', link: '/language/chinese/' },
+              { text: 'Japanese', link: '/language/japanese/' },
+              //删除后非嵌套
+              { text: '分组', items: [
+                { text: 'Chinese', link: '/language/chinese/' },
+                { text: 'Japanese', link: '/language/japanese/' }
+              ] },
+            ]
+          },
+          { text: 'External', link: 'https://google.com' },
+        ],
+        // 多个侧边栏
+      // sidebar: {
+      //   '/css/': [
+      //     '',     /* /foo/ */
+      //     'css1',  /* /foo/one.html */
+      //     'css2'   /* /foo/two.html */
+      //   ],
+  
+      //   '/javascript/': [
+      //     '',      /* /bar/ */
+      //     'javascript1', /* /bar/three.html */
+      //     'javascript2'   /* /bar/four.html */
+      //   ],
+  
+      //   // fallback deny all
+      //   '/': [
+      //     '',        /* / */
+      //     '/about/', /* /contact.html */
+      //     'about/test'    /* /about.html */
+      //   ]
+      // }
+
+      }
+    }
+  
---
+++ docs/about/README.md
@@ -0,0 +1,3 @@
+## 关于页面 <Badge text="beta" type="warning"/> <Badge text="默认主题"/>
+
+哎
