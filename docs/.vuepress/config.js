14aa4d4e54dee9ba4c8d47ce0cf4a1b90a1e62bf
mantoudahanv <mantoudahanv@gmail.com>
2020-03-19 19:33:24 +0800

    vue第一次

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,25 +13,25 @@ module.exports = {
 
     themeConfig: {
       //调整github项目地址
-      repo: 'https://github.com/mantoudahanv/vue.github.io',
-      editLinks: false,
-      editLinkText: '编辑文档！',
+      // repo: 'https://github.com/mantoudahanv/vue.github.io',
+      // editLinks: false,
+      // editLinkText: '编辑文档！',
 
       // 最后更新时间
       // lastUpdated: '上次更新', // string | boolean
       // avbar: false  用于禁用所有页面导航栏
       
-      sidebarDepth: 2,//自动生成侧边栏
+      
         nav: [
           { text: '主页', link: '/' },
           { text: 'Guide', link: '/guide/' },
           {
-            text: 'Languages',
+            text: '其他文档',
             ariaLabel: 'Language Menu',
             //嵌套下拉分组
             items: [
-              { text: 'Chinese', link: '/language/chinese/' },
-              { text: 'Japanese', link: '/language/japanese/' },
+              { text: '直播文档', link: 'css2' },
+              { text: '直播操作', link: 'css1' },
               //删除后非嵌套
               { text: '分组', items: [
                 { text: 'Chinese', link: '/language/chinese/' },
@@ -39,31 +39,31 @@ module.exports = {
               ] },
             ]
           },
-          { text: 'External', link: 'https://google.com' },
         ],
-
         // 多个侧边栏
-      sidebar: {
-        '/css/': [
-          '',     /* /foo/ */
-          'css1',  /* /foo/one.html */
-          'css2'   /* /foo/two.html */
-          
-        ],
+
+  sidebarDepth: 2,
+  sidebar: 'auto'
+      // sidebar: {
+      //   '/css/': [
+      //     '',     
+      //     'css1',  
+      //     'css2',
+      //   ],
   
-        '/javascript/': [
-          '',      /* /bar/ */
-          'javascript1', /* /bar/three.html */
-          'javascript2'   /* /bar/four.html */
-        ],
+      //   '/javascript/': [
+      //     '',      /* /bar/ */
+      //     'javascript1', /* /bar/three.html */
+      //     'javascript2'   /* /bar/four.html */
+      //   ],
   
-        // fallback deny all
-        '/': [
-          '',        /* / */
-          '/about/', /* /contact.html */
-          'about/test'    /* /about.html */
-        ]
-      }
+      //   // fallback deny all
+      //   '/': [
+      //     '',        /* / */
+      //     '/about/', /* /contact.html */
+      //     'about/test'    /* /about.html */
+      //   ]
+      // }
 
       }
     }
